34828f193c58ddb3a9a91fa6de61dc15c83af060
Lrdameron <Lrdameron@gmail.com>
2025-02-02 17:34:26 -0600

    Fixing JavaScript issues

---
+++ JavaScript/logic.js
@@ -0,0 +1,19 @@
+function openTab(evt, tabName) {
+    let tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    let tablinks = document.getElementsByClassName("tablinks");
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+}
+
+// Set default tab to be open
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementsByClassName("tablinks")[0].click();
+});
